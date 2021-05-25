0db1a9475422bc81bc8031a9519e690f79a81f6f
ajaymongia <42540721+ajaymongia@users.noreply.github.com>
2021-05-26 03:33:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './style.css';
+import NavSection from './NavSection';
+import Masthead from './Masthead';
+import AboutSection from './AboutSection';
+import PortfolioSection from './PortfolioSection';
+import ContactSection from './ContactSection';
+import Footer from './Footer';
+ 
+
+
+
+const App = () =>  (
+
+
+        <div>
+                <NavSection />
+                <Masthead />
+                <AboutSection />
+                <PortfolioSection />
+                <ContactSection /> 
+                <Footer /> 
+        </div> 
+
+  
+
+)
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,242 @@
+import React from 'react'
+
+const Footer = () => 
+(
+
+    <div>
+
+    <footer className="footer text-center">
+    <div className="container">
+        <div className="row">
+           
+            <div className="col-lg-4 mb-5 mb-lg-0">
+                <h4 className="text-uppercase mb-4">Location</h4>
+                <p className="lead mb-0">
+                    2215 John Daniel Drive
+                    <br />
+                    Clark, MO 65243
+                </p>
+            </div>
+           
+            <div className="col-lg-4 mb-5 mb-lg-0">
+                <h4 className="text-uppercase mb-4">Around the Web</h4>
+                <a className="btn btn-outline-light btn-social mx-1" href="#!"><i className="fab fa-fw fa-facebook-f"></i></a>
+                <a className="btn btn-outline-light btn-social mx-1" href="#!"><i className="fab fa-fw fa-twitter"></i></a>
+                <a className="btn btn-outline-light btn-social mx-1" href="#!"><i className="fab fa-fw fa-linkedin-in"></i></a>
+                <a className="btn btn-outline-light btn-social mx-1" href="#!"><i className="fab fa-fw fa-dribbble"></i></a>
+            </div>
+           
+            <div className="col-lg-4">
+                <h4 className="text-uppercase mb-4">About Freelancer</h4>
+                <p className="lead mb-0">
+                    Freelance is a free to use, MIT licensed Bootstrap theme created by
+                    <a href="http://startbootstrap.com">Start Bootstrap</a>
+                    .
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+ 
+<div className="copyright py-4 text-center text-white">
+    <div className="container"><small>Copyright &copy; Your Website 2021</small></div>
+</div>
+ 
+ 
+<div className="portfolio-modal modal fade" id="portfolioModal1"  aria-labelledby="portfolioModal1" aria-hidden="true">
+    <div className="modal-dialog modal-xl">
+        <div className="modal-content">
+            <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+            <div className="modal-body text-center pb-5">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-8">
+                           
+                            <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Log Cabin</h2>
+                           
+                            <div className="divider-custom">
+                                <div className="divider-custom-line"></div>
+                                <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                                <div className="divider-custom-line"></div>
+                            </div>
+                           
+                            <img className="img-fluid rounded mb-5" src="https://startbootstrap.github.io/startbootstrap-freelancer/assets/img/portfolio/cabin.png" alt="..." />
+                           
+                            <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                            <button className="btn btn-primary" href="#!" data-bs-dismiss="modal">
+                                <i className="fas fa-times fa-fw"></i>
+                                Close Window
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+ 
+<div className="portfolio-modal modal fade" id="portfolioModal2"  aria-labelledby="portfolioModal2" aria-hidden="true">
+    <div className="modal-dialog modal-xl">
+        <div className="modal-content">
+            <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+            <div className="modal-body text-center pb-5">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-8">
+                           
+                            <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Tasty Cake</h2>
+                           
+                            <div className="divider-custom">
+                                <div className="divider-custom-line"></div>
+                                <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                                <div className="divider-custom-line"></div>
+                            </div>
+                           
+                            <img className="img-fluid rounded mb-5" src="https://startbootstrap.github.io/startbootstrap-freelancer/assets/img/portfolio/cake.png" alt="..." />
+                           
+                            <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                            <button className="btn btn-primary" href="#!" data-bs-dismiss="modal">
+                                <i className="fas fa-times fa-fw"></i>
+                                Close Window
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+ 
+<div className="portfolio-modal modal fade" id="portfolioModal3"  aria-labelledby="portfolioModal3" aria-hidden="true">
+    <div className="modal-dialog modal-xl">
+        <div className="modal-content">
+            <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+            <div className="modal-body text-center pb-5">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-8">
+                           
+                            <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Circus Tent</h2>
+                           
+                            <div className="divider-custom">
+                                <div className="divider-custom-line"></div>
+                                <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                                <div className="divider-custom-line"></div>
+                            </div>
+                           
+                            <img className="img-fluid rounded mb-5" src="https://startbootstrap.github.io/startbootstrap-freelancer/assets/img/portfolio/circus.png" alt="..." />
+                           
+                            <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                            <button className="btn btn-primary" href="#!" data-bs-dismiss="modal">
+                                <i className="fas fa-times fa-fw"></i>
+                                Close Window
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+ 
+<div className="portfolio-modal modal fade" id="portfolioModal4"  aria-labelledby="portfolioModal4" aria-hidden="true">
+    <div className="modal-dialog modal-xl">
+        <div className="modal-content">
+            <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+            <div className="modal-body text-center pb-5">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-8">
+                           
+                            <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Controller</h2>
+                           
+                            <div className="divider-custom">
+                                <div className="divider-custom-line"></div>
+                                <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                                <div className="divider-custom-line"></div>
+                            </div>
+                           
+                            <img className="img-fluid rounded mb-5" src="https://startbootstrap.github.io/startbootstrap-freelancer/assets/img/portfolio/game.png" alt="..." />
+                           
+                            <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                            <button className="btn btn-primary" href="#!" data-bs-dismiss="modal">
+                                <i className="fas fa-times fa-fw"></i>
+                                Close Window
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+ 
+<div className="portfolio-modal modal fade" id="portfolioModal5"  aria-labelledby="portfolioModal5" aria-hidden="true">
+    <div className="modal-dialog modal-xl">
+        <div className="modal-content">
+            <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+            <div className="modal-body text-center pb-5">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-8">
+                           
+                            <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Locked Safe</h2>
+                           
+                            <div className="divider-custom">
+                                <div className="divider-custom-line"></div>
+                                <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                                <div className="divider-custom-line"></div>
+                            </div>
+                           
+                            <img className="img-fluid rounded mb-5" src="https://startbootstrap.github.io/startbootstrap-freelancer/assets/img/portfolio/safe.png" alt="..." />
+                           
+                            <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                            <button className="btn btn-primary" href="#!" data-bs-dismiss="modal">
+                                <i className="fas fa-times fa-fw"></i>
+                                Close Window
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+ 
+<div className="portfolio-modal modal fade" id="portfolioModal6"  aria-labelledby="portfolioModal6" aria-hidden="true">
+    <div className="modal-dialog modal-xl">
+        <div className="modal-content">
+            <div className="modal-header border-0"><button className="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+            <div className="modal-body text-center pb-5">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-8">
+                           
+                            <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">Submarine</h2>
+                           
+                            <div className="divider-custom">
+                                <div className="divider-custom-line"></div>
+                                <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                                <div className="divider-custom-line"></div>
+                            </div>
+                          
+                            <img className="img-fluid rounded mb-5" src="https://startbootstrap.github.io/startbootstrap-freelancer/assets/img/portfolio/submarine.png" alt="..." />
+                           
+                            <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                            <button className="btn btn-primary" href="#!" data-bs-dismiss="modal">
+                                <i className="fas fa-times fa-fw"></i>
+                                Close Window
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+
+);
+
+export default  Footer;
